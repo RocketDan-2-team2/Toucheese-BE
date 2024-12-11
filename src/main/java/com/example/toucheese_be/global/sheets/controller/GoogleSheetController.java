209fea6c954c30dd5328abe65230772ab7f46dbb
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-12-11 11:38:59 +0900

    feat/ET-137 컨:트롤러ì 정

--- src/main/java/com/example/toucheese_be/global/sheets/controller/GoogleSheetController.java
+++ src/main/java/com/example/toucheese_be/global/sheets/controller/GoogleSheetController.java
@@ -2,26 +2,44 @@ package com.example.toucheese_be.global.sheets.controller;
 
 import com.example.toucheese_be.global.sheets.service.GoogleSheetsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sheets")
 public class GoogleSheetController {
 
-    @Autowired
     private GoogleSheetsService googleSheetsService;
 
-    @GetMapping("/read")
-    public List<List<Object>> readSheets(@RequestParam String range) throws IOException {
-        return googleSheetsService.readSheets(range);
-    }
+    // 기존 시트id를 복사해둔 곳을 이 Url에 저장
+    private static final String SPREADSHEET_ID = "https://docs.google.com/spreadsheets/d/1zZu34Qb3UfO6JkqGkynefgb1uLkvRrGUj_VktiDKe9A/edit?gid=0#gid=0";
+
+    // 작성할 행 입력 Ex) A1
+    private static final String RANGE = "A1";
 
     @PostMapping("/write")
-    public void writeSheets(@RequestBody List<List<Object>> values, @RequestParam String range) throws IOException{
-        googleSheetsService.writeSheets(values, range);
+    public ResponseEntity<String> writeToSheets(@RequestParam String word){
+        try {
+
+            // 데이터를 스프레드시트에 쓰기 위해 전달되는 형식
+            // 행과 열에 데이터를 매핑하기 위함
+            List<List<Object>> values = List.of(Collections.singletonList(word));
+
+            googleSheetsService.writeToSheets(SPREADSHEET_ID, RANGE, values);
+
+            return ResponseEntity.ok("데이터가 입력되었습니다. : " + word);
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+            return ResponseEntity.internalServerError().body("데이터 입력에 실패하였습니다:" + e.getMessage());
+        }
+
     }
 
 }
--- src/main/java/com/example/toucheese_be/global/sheets/service/GoogleSheetsService.java
+++ src/main/java/com/example/toucheese_be/global/sheets/service/GoogleSheetsService.java
@@ -5,17 +5,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class GoogleSheetsService {
 
-    public List<List<Object>> readSheets(String range) {
 
-    }
-
-    public void writeSheets(List<List<Object>> values, String range) {
-
+    public void writeToSheets(String spreadsheetId, String range, List<List<Object>> values) {
     }
 }
